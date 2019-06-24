afd746572bb184bb0a0d009f1d09b3ddac0606a3
sandhya r <mail.sandhyaravi@gmail.com>
2019-06-24 11:38:57 +0530

    commit-04

---
+++ scripts/cluster-kube.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+sleep 20
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
