593ae6528fd6bcf14b3ee9d50843caa25ad066d8
sandhya r <mail.sandhyaravi@gmail.com>
2019-06-25 14:58:03 +0530

    travis changes test002

--- scripts/cluster-kube.sh
+++ scripts/cluster-kube.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
-apt-get update && apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
-
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+kubeadm reset
+y
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-
 sleep 20
-
+rm -rf $HOME/.kube/
 mkdir -p $HOME/.kube
+sudo chmod 777 /etc/kubernetes/admin.conf
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chmod 777 $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
 kubectl taint nodes --all node-role.kubernetes.io/master-
-
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -f https://raw.githubusercontent.com/cni-genie/CNI-Genie/master/conf/1.8/genie-complete.yaml
