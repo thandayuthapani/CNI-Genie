51473ca88f7a81695d2f1eee592b86cd9dc57994
sandhya r <mail.sandhyaravi@gmail.com>
2019-06-26 15:37:35 +0530

    commit-travis-test-02

--- scripts/cluster-kube.sh
+++ scripts/cluster-kube.sh
@@ -3,10 +3,10 @@
 sudo apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+# update
 sudo apt-get update
 
-sudo swapoff -a
-
+# Install kubeadm
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo kubeadm reset -f
