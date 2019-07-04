d866d46a09bfd0a2399306f7c68bdc8b14e8b15a
sandhya r <mail.sandhyaravi@gmail.com>
2019-07-04 11:35:52 +0530

    Example-Travis-E2E-Test

--- e2e/cni-genie_k8s_test.go
+++ e2e/cni-genie_k8s_test.go
@@ -56,7 +56,7 @@ var _ = Describe("CNIGenie", func() {
 			interfaceName := "eth0"
 			glog.Info(interfaceName)
 
-			FIt("should succeed calico networking for pod", func() {
+			It("should succeed calico networking for pod", func() {
 				annots := make(map[string]string)
 				annots["cni"] = "calico"
 				//Create a K8s Pod with calico cni
@@ -105,6 +105,7 @@ var _ = Describe("CNIGenie", func() {
 			glog.Info(interfaceName)
 
 			It("should succeed romana networking for pod", func() {
+				Skip("Skipped Adding romana networking for Pod")
 				annots := make(map[string]string)
 				annots["cni"] = "romana"
 				//Create a K8s Pod with calico cni
@@ -152,7 +153,7 @@ var _ = Describe("CNIGenie", func() {
 			interfaceName := "eth0"
 			glog.Info(interfaceName)
 
-			FIt("should succeed weave networking for pod", func() {
+			It("should succeed weave networking for pod", func() {
 				annots := make(map[string]string)
 				annots["cni"] = "weave"
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
@@ -199,7 +200,7 @@ var _ = Describe("CNIGenie", func() {
 			interfaceName := "eth0"
 			glog.Info(interfaceName)
 
-			FIt("should succeed multi-ip networking for pod", func() {
+			It("should succeed multi-ip networking for pod", func() {
 				annots := make(map[string]string)
 				annots["cni"] = "flannel,weave"
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
@@ -247,6 +248,7 @@ var _ = Describe("CNIGenie", func() {
 			glog.Info(interfaceName)
 
 			It("should succeed nocni networking for pod", func() {
+				Skip("skip Adding nocni networking for Pod")
 				annots := make(map[string]string)
 				annots["cni"] = " "
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
@@ -290,7 +292,7 @@ var _ = Describe("CNIGenie", func() {
 		Context("using cni-genie for configuring bridge CNI", func() {
 			name := fmt.Sprintf("nginx-bridge-%d", rand.Uint32())
 
-			FIt("should succeed bridge networking for pod", func() {
+			It("should succeed bridge networking for pod", func() {
 				annots := make(map[string]string)
 				annots["cni"] = "bridge"
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
@@ -336,7 +338,7 @@ var _ = Describe("CNIGenie", func() {
 			interfaceName := "eth0"
 			glog.Info(interfaceName)
 
-			FIt("should succeed multi-ip (weave, bridge) networking for pod", func() {
+			It("should succeed multi-ip (weave, bridge) networking for pod", func() {
 				annots := make(map[string]string)
 				annots["cni"] = "weave,bridge"
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
@@ -380,7 +382,7 @@ var _ = Describe("CNIGenie", func() {
 		Context("using cni-genie for configuring macvlan CNI", func() {
 			name := fmt.Sprintf("nginx-macvlan-%d", rand.Uint32())
 
-			FIt("should succeed macvlan networking for pod", func() {
+			It("should succeed macvlan networking for pod", func() {
 				annots := make(map[string]string)
 				annots["cni"] = "macvlan"
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
@@ -424,7 +426,7 @@ var _ = Describe("CNIGenie", func() {
 		Context("using cni genie to get multiple Ip and update in annotation", func() {
 			name := fmt.Sprintf("nginx-multiip-pref-%d", rand.Uint32())
 
-			FIt("should succeed multi ip preference for pod", func() {
+			It("should succeed multi ip preference for pod", func() {
 				annots := make(map[string]string)
 				annots["cni"] = "weave,flannel"
 
@@ -470,6 +472,7 @@ var _ = Describe("CNIGenie", func() {
 			name := fmt.Sprintf("nginx-sriov-%d", rand.Uint32())
 
 			It("should succeed sriov networking for pod", func() {
+				Skip("Skip Adding sriov networking for Pod")
 				annots := make(map[string]string)
 				annots["cni"] = "sriov"
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
@@ -514,7 +517,7 @@ var _ = Describe("CNIGenie", func() {
 		Context("using cni genie to configure multiple ip from flannel plugin", func() {
 			name := fmt.Sprintf("nginx-multiip-from-flannel-%d", rand.Uint32())
 
-			FIt("should succeed multi ip preference for pod", func() {
+			It("should succeed multi ip preference for pod", func() {
 				annots := make(map[string]string)
 				annots["cni"] = "flannel,flannel"
 
@@ -560,7 +563,7 @@ var _ = Describe("CNIGenie", func() {
 		Context("using cni genie to configure multiple ip from flannel plugin and weave plugin", func() {
 			name := fmt.Sprintf("nginx-multiip--%d", rand.Uint32())
 
-			FIt("should succeed multi ip preference for pod", func() {
+			It("should succeed multi ip preference for pod", func() {
 				annots := make(map[string]string)
 				annots["cni"] = "flannel,weave,flannel"
 
@@ -606,7 +609,7 @@ var _ = Describe("CNIGenie", func() {
 		Context("using cni-genie for verifying default plugin case : pod with no annotation attributes", func() {
 			name := fmt.Sprintf("nginx-pod-no-annotation-%d", rand.Uint32())
 
-			FIt("should succeed default(weave) networking for pod", func() {
+			It("should succeed default(weave) networking for pod", func() {
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
 					ObjectMeta: metav1.ObjectMeta{
 						Name: name,
@@ -649,7 +652,7 @@ var _ = Describe("CNIGenie", func() {
 		Context("using cni-genie for verifying default plugin case : pod with non cni annotations", func() {
 			name := fmt.Sprintf("nginx-pod-non-cni-annotation-%d", rand.Uint32())
 
-			FIt("should succeed default(weave) networking for pod", func() {
+			It("should succeed default(weave) networking for pod", func() {
 				annots := make(map[string]string)
 				annots["build"] = "two"
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
@@ -694,7 +697,7 @@ var _ = Describe("CNIGenie", func() {
 		Context("using cni-genie for verifying default plugin case : pod with blank annotation field", func() {
 			name := fmt.Sprintf("nginx-pod-blank-annotation-%d", rand.Uint32())
 
-			FIt("should succeed default(weave) networking for pod", func() {
+			It("should succeed default(weave) networking for pod", func() {
 				annots := make(map[string]string)
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
 					ObjectMeta: metav1.ObjectMeta{
@@ -738,7 +741,7 @@ var _ = Describe("CNIGenie", func() {
 		Context("using cni-genie for verifying plugin with interface name case : pod with plugin+interface name case", func() {
 			name := fmt.Sprintf("nginx-pod-with-ifname-%d", rand.Uint32())
 
-			FIt("should succeed multinetworking with ifname for pod", func() {
+			It("should succeed multinetworking with ifname for pod", func() {
 				annots := make(map[string]string)
 				annots["cni"] = "flannel,weave@eth4,flannel@eth5, flannel"
 				_, err := clientset.CoreV1().Pods(TEST_NAMESPACE).Create(&v1.Pod{
@@ -778,7 +781,7 @@ var _ = Describe("CNIGenie", func() {
 		})
 	})
 	Describe("To create logical network crd objects ", func() {
-		FIt("should succeed crd creation", func() {
+		It("should succeed crd creation", func() {
 			config, err := clientcmd.BuildConfigFromFlags("", testKubeConfig)
 			apiextensionsclient, err = apiextensionsclientset.NewForConfig(config)
 			if err != nil {
@@ -822,7 +825,7 @@ var _ = Describe("CNIGenie", func() {
 			Expect("Success").To(Equal("Success"))
 		})
 
-		FIt("should succeed weave networking for pod", func() {
+		It("should succeed weave networking for pod", func() {
 			glog.Info("Add weave networking for Pod by specifying logical network")
 			name := fmt.Sprintf("nginx-weave-%d", rand.Uint32())
 			interfaceName := "eth0"
@@ -855,7 +858,7 @@ var _ = Describe("CNIGenie", func() {
 	})
 
 	Describe("To create NetworkAttachmentDefinition CRD ", func() {
-		FIt("should succeed crd creation", func() {
+		It("should succeed crd creation", func() {
 			config, err := clientcmd.BuildConfigFromFlags("", testKubeConfig)
 			apiextensionsclient, err = apiextensionsclientset.NewForConfig(config)
 			if err != nil {
@@ -896,7 +899,7 @@ var _ = Describe("CNIGenie", func() {
 			Expect("Success").To(Equal("Success"))
 		})
 
-		FIt("To create NetworkAttachmentDefinition object flannel", func() {
+		It("To create NetworkAttachmentDefinition object flannel", func() {
 			config, err := clientcmd.BuildConfigFromFlags("", testKubeConfig)
 			netattachclient, err := netattachclientset.NewForConfig(config)
 			if err != nil {
@@ -917,7 +920,7 @@ var _ = Describe("CNIGenie", func() {
 		})
 
 		//Create NetworkAttachmentDefinition object with plugin configuration as file
-		FIt("To create NetworkAttachmentDefinition object weave", func() {
+		It("To create NetworkAttachmentDefinition object weave", func() {
 			config, err := clientcmd.BuildConfigFromFlags("", testKubeConfig)
 			netattachclient, err := netattachclientset.NewForConfig(config)
 			if err != nil {
@@ -933,7 +936,7 @@ var _ = Describe("CNIGenie", func() {
 		})
 
 		//flannel, weave networking for Pod
-		FIt("should succeed weave networking for pod", func() {
+		It("should succeed weave networking for pod", func() {
 			glog.Info("Inside Check for adding weave-flannel networking")
 			name := fmt.Sprintf("nginx-netattachdef-flannel-weave-%d", rand.Uint32())
 			interfaceName := "eth0"
