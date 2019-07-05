266ee00b999e700567e7d7ffe8b85561d2b469a2
sandhya r <mail.sandhyaravi@gmail.com>
2019-07-05 12:09:56 +0530

    e2e travis test sample-01

--- e2e/scripts/e2e.sh
+++ e2e/scripts/e2e.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-#bash -x ../../plugins_install.sh -all
+#Install all plugins
+bash -x ../../plugins_install.sh -all
 
-#sleep 10
+sleep 10
+cd ../..
 
 make test-e2e testKubeVersion=1.7 testKubeConfig=/etc/kubernetes/admin.conf
 
 sleep 20
 
-
+bash -x ../../plugins_install.sh -deleteall
